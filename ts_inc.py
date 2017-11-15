6634fe156e4c1c60172e5541f200509304e06de6
ehavazli <emre.havazli@gmail.com>
2017-11-15 13:26:08 -0500

    added option to calcute the differences between chosen epochs or the whole time series bug fix for overweriting to new generated files changed naming convention, now created files are named as the original file by adding '_inc' (if the whole time series incrementals are calculated) or '_diff' if a list of dates are given.

--- ts_inc.py
+++ ts_inc.py
@@ -10,20 +10,28 @@ import pysar._datetime as ptime
 def main(argv):
   try:
     timeseries_file = argv[1]
-    print timeseries_file
+    epochList = argv[2]
+    print 'EPOCHS: ' + epochList
+    if epochList == 'all':
+        epochs = 'all'
+    else:
+        epochs = argv[2].split()
+        print 'Epochs to be subtracted: '+ str(epochs)
+
   except:
     print '''
         *******************************************
            Calculate incremental changes between timeseries epochs
 
-           Usage: ts_inc.py [timeseries_file]
+           Usage: ts_inc.py [timeseries_file] [epochs or all]
+
+           ts_inc.py timeseries_tropHgt_demErr_refDate.h5 '20170725 20170824 20170923 20171023'
+           ts_inc.py timeseries_tropHgt_demErr_refDate.h5 'all'
 
         *******************************************
         '''
     sys.exit(1)
 
-
-#  timeseries_file = 'timeseries.h5'
   atr = readfile.read_attribute(timeseries_file)
   k = atr['FILE_TYPE']
   print 'input '+k+' file: '+timeseries_file
@@ -31,37 +39,74 @@ def main(argv):
     sys.exit('ERROR: input file is not timeseries!')
   h5file = h5py.File(timeseries_file)
 
+  if epochs == 'all':
 #####################################
   ## Date Info
-  dateListAll = sorted(h5file[k].keys())
-  print '--------------------------------------------'
-  print 'Dates from input file: '+str(len(dateListAll))
-  print dateListAll
+      dateListAll = sorted(h5file[k].keys())
+      print '--------------------------------------------'
+      print 'Dates from input file: '+str(len(dateListAll))
+      print dateListAll
 
-  dateNum = len(dateListAll)
-  h5file_inc = 'timeseries_inc.h5'
-  f = h5py.File(h5file_inc)
-  gg = f.create_group('timeseries')
+      dateNum = len(dateListAll)
+      h5file_inc = timeseries_file[0:-3]+'_inc.h5'
+      f = h5py.File(h5file_inc,'w')
+      gg = f.create_group('timeseries')
 
-  width = int(atr['WIDTH'])
-  length = int(atr['FILE_LENGTH'])
+      width = int(atr['WIDTH'])
+      length = int(atr['FILE_LENGTH'])
 
-  for i in range(dateNum):
-      if i+1 >= dateNum:
-          print 'End of time series file'
-      else:
-          date_1 = dateListAll[i]
-          date_2 = dateListAll[i+1]
-          title = str(date_2)+'-'+str(date_1)
-          print 'Calculating '+ title
-          ts_1 = h5file[k].get(date_1)[:].flatten()
-          ts_2 = h5file[k].get(date_2)[:].flatten()
-          inc = ts_2 - ts_1
-          inc = inc.reshape(length,width)
-          dset = gg.create_dataset(title, data=inc, compression='gzip')
+      for i in range(dateNum):
+          if i+1 >= dateNum:
+              print 'End of time series file'
+          else:
+              date_1 = dateListAll[i]
+              date_2 = dateListAll[i+1]
+              title = str(date_2)+'-'+str(date_1)
+              print 'Calculating '+ title
+              ts_1 = h5file[k].get(date_1)[:].flatten()
+              ts_2 = h5file[k].get(date_2)[:].flatten()
+              inc = ts_2 - ts_1
+              inc = inc.reshape(length,width)
+              dset = gg.create_dataset(title, data=inc, compression='gzip')
 
-          for key , value in atr.iteritems():
-              gg.attrs[key]=value
+              for key , value in atr.iteritems():
+                  gg.attrs[key]=value
+
+  elif len(epochs) > 1:
+      h5file_diff = timeseries_file[0:-3]+'_diff.h5'
+      f = h5py.File(h5file_diff,'w')
+      gg = f.create_group('timeseries')
+      width = int(atr['WIDTH'])
+      length = int(atr['FILE_LENGTH'])
+
+      dateNum = len(epochs)
+      for i in range(dateNum):
+          if i+1 >= dateNum:
+              print 'End of date list'
+          else:
+              date_1 = epochs[i]
+              date_2 = epochs[i+1]
+              title = str(date_2)+'-'+str(date_1)
+              print 'Calculating '+ title
+              ts_1 = h5file[k].get(date_1)[:].flatten()
+              ts_2 = h5file[k].get(date_2)[:].flatten()
+              inc = ts_2 - ts_1
+              inc = inc.reshape(length,width)
+              dset = gg.create_dataset(title, data=inc, compression='gzip')
+
+              for key , value in atr.iteritems():
+                  gg.attrs[key]=value
+  else:
+            print '''
+                *******************************************
+                   Calculate incremental changes between timeseries epochs
+
+                   Usage: ts_inc.py [timeseries_file] [epochs or all]
+
+                *******************************************
+                '''
+            sys.exit(1)
+
 
 
 
