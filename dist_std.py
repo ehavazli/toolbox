3f1b4c664955686357e26e431e298494f8774aa5
ehavazli <emre.havazli@gmail.com>
2018-02-21 11:21:08 -0500

    save the screen

--- dist_std.py
+++ dist_std.py
@@ -6,6 +6,7 @@ import sys
 from numpy import *
 import glob
 import h5py
+import random
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
@@ -14,15 +15,15 @@ from matplotlib.pyplot import cm
 def main(argv):
     try:
         directory = argv[0]
-        s_y = int(argv[1])
-        s_x = int(argv[2])
-        e_y = int(argv[3])
-        e_x = int(argv[4])
+        e_y = int(argv[1])
+        e_x = int(argv[2])
     except:
         print '''
     *******************************************
 
-       Usage: dist_std.py [directory] [start-y] [start-x] [end-y] [end-x]
+       Usage: dist_std.py [directory] [end_y] [end_x]
+       e_y = end iteration at this y
+       e_x = end iteration at this x
 
     *******************************************
     '''
@@ -31,27 +32,72 @@ def main(argv):
     files = sorted(glob.glob(directory+'/average_std_*_years.h5'))
     x_res = float(183.25)
     y_res = float(333)
-    color=iter(cm.jet(linspace(0,1,8)))
+    std_pix = {}
     for k in files:
         file_name = k.split('_')[-2:]
         years = file_name[0]
+        print 'Working on '+str(years)+' years long time series'
         f = h5py.File(k,'r')
         dset = f['velocity'].get('velocity')
-        std = asarray(dset)
-        ref_std = std[s_y][s_x]
-        std_pix = []
+        stda = asarray(dset)
+        std_lst = []
+        dist = []
+        for i in xrange(0,10):
+            s_y = random.randint(0,999)
+            s_x = random.randint(0,999)
+            ref_std = stda[s_y][s_x]
+            # print str(s_y)+' '+str(s_x)
+            for y in xrange(0,e_y):
+                for x in xrange(0,e_x):
+                    std_val = abs(stda[y][x]-ref_std)
+                    std_lst.append(std_val)
+                    dist.append(sqrt(((s_y-y)*y_res)**2+((s_x-x)*x_res)**2))
+        # print years+' '+str(len(std_lst))+' '+str(len(dist))
+        std_pix[int(years)] = [std_lst,dist]
+
+    x = arange(0,360000,10000)
+    color=iter(cm.jet(linspace(0,1,8)))
+
+    for key, value in sorted(std_pix.iteritems()):
+        print 'Working on plots of '+str(key)+' years data'
         c=next(color)
-        for y in xrange(s_y,e_y):
-            for x in xrange(s_x,e_x):
-                std_val = std[y][x]-ref_std
-                std_pix.append(std_val)
-                dist = sqrt(((s_y-y)*y_res)**2+((s_x-x)*x_res)**2)
-                plt.ylabel('Uncertainty (mm/yr)')
-                plt.xlabel('Distance (km)')
-                plt.axis([0, 350,0, 2])
-                plt.plot(dist/1000.0,std_val*10000.0, '*',c=c)
-    plt.savefig(directory+'/STDvsDIST.png',bbox_inches="tight")
-    plt.close()
+        avg = []
+        avg_std=[]
+        avg_er = []
+        for n in xrange(0,35):
+            print 'Distances between '+str(x[n])+' and '+str(x[n+1])
+            plt.ylabel('Uncertainty (mm/yr)')
+            plt.xlabel('Distance (km)')
+            for i, q in enumerate(value[1]):
+                if x[n] <= value[1][i] <= x[n+1]:
+                    avg_std.append(value[0][i])
+            avg.append(nanmean(avg_std))
+            avg_er.append(std(avg_std))
+        avg.insert(0,0)
+        avg_er.insert(0,0)
+        # print 'X: '+str(x)
+        # print 'AVG: '+str(avg)
+        # print 'AVG_ER: '+str(avg_er)
+        # plt.scatter(x/1000.0,array(avg)*10000.0,c=c,label=(str(key)+' years'))
+        plt.errorbar(x/1000.0,array(avg)*100000.0,array(avg_er)*100000.0,c=c,marker='o',xerr = None,ls='none',label=(str(key)+' years'))
+    plt.legend()
+    plt.savefig(directory+'/STDvsDIST.png',bbox_inches="tight",dpi=600)
+
+
+
+
+
+#     color=iter(cm.jet(linspace(0,1,8)))
+#     for key, value in sorted(std_pix.iteritems()):
+#         c=next(color)
+#         plt.ylabel('Uncertainty (mm/yr)')
+#         plt.xlabel('Distance (km)')
+# #        plt.axis([0, 350,0,2])
+#         plots = plt.plot(array(value[1])/1000.0,array(value[0])*10000.0, '*',c=c,label=(str(key)+' years'))
+# #        plt.plot(sol,c=c,label=(str(key)+' years'))
+#         plt.legend()
+#     plt.savefig(directory+'/STDvsDIST.png',bbox_inches="tight",dpi=600)
+# #    plt.close()
 
             # out_file = open(directory+'std_dst_'+years+'_'+str(y)+'_'+str(s_y)+'-''+str(s_x)+'.txt', 'w')
             # std_pix_ar = array(std_pix)*float(10000.0)
