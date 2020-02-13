fixed bug in incidence angle conversion from radians to degrees

los2upTS.py
@@ -19,7 +19,6 @@ def createParser():
     import argparse
     parser = argparse.ArgumentParser(description='LOS to UP')
     parser.add_argument('-g', '--geoFile', dest='geoFile', type=str, default='./inputs/geometryGeo.h5', help='Path and name of the geomertyGeo.h5 file (default: ./inputs/geomertyGeo.h5)')
-    # parser.add_argument('-v', '--velocity', dest='velFile', type=str, default='./velocity.h5', help='Path and name of the velocity file (default: ./velocity.h5)')
     parser.add_argument('-t', '--timeseries', dest='tsFile', type=str, default='./timeseries.h5', help='Path and name of the timeseries file (default: ./timeseries.h5)')
     parser.add_argument('-o', '--outname', dest='outName', type=str, default='./velocity_up.h5', help='Path and name of the output file (default: ./velocity_up.h5)')
     return parser
@@ -32,16 +31,13 @@ def main(inps=None):
     inps = cmdLineParse()
 
     h5Geo = h5py.File(inps.geoFile,'r')
-    # h5Vel = h5py.File(inps.velFile,'r')
     h5ts = h5py.File(inps.tsFile,'r')
     h5Out = h5py.File(inps.outName, 'w')
 
     Iset = h5Geo.get('incidenceAngle').value
-    Inc = Iset**(np.pi/180.)
-    # attrs = h5Vel.attrs
+    Inc = Iset*(np.pi/180.)
     attrs = h5ts.attrs
 
-    # Vset = h5Vel.get('velocity')
     Vset = h5ts.get('timeseries')
     bperp = h5ts.get('bperp')
     dates = h5ts.get('date')
@@ -52,16 +48,11 @@ def main(inps=None):
     dset = h5Out.create_dataset('date',data=dates)
     dset = h5Out.create_dataset('timeseries', data=losU)
 
-
-    # for key, value in h5Vel.attrs.items():
-    #     dset.attrs[key] = value
     for key, value in h5ts.attrs.items():
         dset.attrs[key] = value
     h5Out.close()
     h5Geo.close()
-    # h5Vel.close()
     h5ts.close()
-
 ############################################################################
 if __name__ == '__main__':
     main()